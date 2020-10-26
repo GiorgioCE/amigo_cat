8ce94ce657f7dfa4f1e34a253b9c71e5a6890c17
Joseph <jpkershaw17@gmail.com>
2020-10-26 17:19:26 +0000

    Using forms

--- app.rb
+++ app.rb
@@ -5,8 +5,13 @@ get '/' do
   'hello'
 end
 
+# get '/random-cat' do
+#   @name = ["Amigo", "Oscar", "Viking"].sample
+#   erb(:index)
+# end
 
-get '/joe' do
-  @name = ["Amigo", "Oscar", "Viking"].sample
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
